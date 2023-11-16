98c85dc6838f29462737368f77a73966cd788e46
LanStarD <853304398>
2023-11-16 17:53:44 +0800

    添加颜色转换函数

--- server_scripts/area_check.js
+++ server_scripts/area_check.js
@@ -33,7 +33,21 @@ function deviation(x,p1,p2,d) {//偏移函数
 }
 
 
+function getGradientColor(value) {
+    if (value < 0 || value > 5) {
+        throw new Error('Value must be between 0 and 5');
+    }
 
+    // 线性插值计算红色和绿色的强度
+    const red = Math.min(255, Math.floor(255 * value / 5));
+    const green = Math.min(255, Math.floor(255 * (1 - value / 5)));
+
+    // 将RGB值转换为十六进制代码
+    const redHex = red.toString(16).padStart(2, '0');
+    const greenHex = green.toString(16).padStart(2, '0');
+
+    return `#${redHex}${greenHex}00`; // 固定蓝色分量为0
+}
 
 
 
