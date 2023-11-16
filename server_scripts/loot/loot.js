835a41a1feed30bf64b450599ed8f7bf8b22101b
SGSxingchen <853304398>
2023-11-16 22:30:28 +0800

    设置废铁获取

---
+++ server_scripts/loot/loot.js
@@ -0,0 +1,19 @@
+var ETypeMob = [
+    "zombie_extreme:infected",
+    "zombie_extreme:crawler",
+    "zombie_extreme:runner",
+    "rottencreatures:frostbitten",
+    "minecraft:zombie",
+    "minecraft:husk",
+    "zombie_extreme:infected_police"
+  ]
+//怪物掉铁
+onEvent("lootjs", (event) => {
+    for(var mob in ETypeMob){
+        event
+        .addEntityLootModifier(mob)
+        .randomChance(0.1) // 10% 概率
+        .thenAdd("kubejs:scarpiron");
+    }
+    
+});
