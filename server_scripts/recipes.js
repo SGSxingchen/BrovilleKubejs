cfac0e9cc8ae9f272e257a17ff4bc909561da596
YakumoUUZ <122345646+YakumoUUZ@users.noreply.github.com>
2023-11-16 16:05:16 +0800

    Update recipes.js

--- server_scripts/recipes.js
+++ server_scripts/recipes.js
@@ -5,21 +5,31 @@ settings.logErroringRecipes = true
 
 var tiers = ['wooden','stone']
 var tools = ['sword',"shovel",'pickaxe','axe','hoe']
+var banItem = ['zombiekit:nitrocellulose', 'zombiekit:cloth','minecraft:enchanting_table','hardcore_torches:lit_torch']
+var banMod = ['zombie_extreme']
 
 onEvent('item.tags',event =>{
     event.add('forge:tools/axes','kubejs:scarpiron_axe')
 })
 
-
-
 onEvent('recipes', event =>{
-    event.remove({output:'zombiekit:fibre'})
+    for (const i in banItem) {
+        event.remove({output:banItem[i]})
+    }
+    
+    for (const i in banMod) {
+        event.remove({mod:banMod[i]})
+    }
 
     for (const ti in tiers) {
         for (const to in tools) {
             event.remove({output:`minecraft:${tiers[ti]}_${tools[to]}`})
         }
     }
+})
+
+onEvent('recipes', event =>{
+
 
     event.shaped('hardcore_torches:unlit_torch',[
         'F',
@@ -29,13 +39,14 @@ onEvent('recipes', event =>{
         S:'#forge:rods/wooden'
     })
 
+    event.remove({output:'zombiekit:fibre'})
     event.shapeless('zombiekit:fibre', ['3x farmersdelight:straw'])
 
     event.shaped('kubejs:scarpiron_axe',[
         "FI",
         "S "
     ],{
-        F:'zombiekit:fibre',
+        F:'farmersdelight:rope',
         I:'kubejs:scarpiron',
         S:'#forge:rods/wooden'
     })
@@ -45,7 +56,7 @@ onEvent('recipes', event =>{
         "FI",
         "S "
     ],{
-        F:'zombiekit:fibre',
+        F:'farmersdelight:rope',
         I:'minecraft:flint',
         S:'#forge:rods/wooden'
     })
@@ -100,4 +111,32 @@ onEvent('recipes', event =>{
 			"count":1
 		  }]
 	})
+    event.remove({id:"farmersdelight:lead_from_straw"})
+    event.remove({id:"farmersdelight:rope"})
+
+    event.shapeless('2x farmersdelight:rope',['3x zombiekit:fibre'])
+    event.custom({
+        "type": "minecraft:campfire_cooking",
+        "ingredient": {
+          "item": "kubejs:treebark_cup_water"
+        },
+        "result": "kubejs:treebark_cup_purewater",
+        "experience": 0.35,
+        "cookingtime": 600
+    })//烧树皮杯装水
+    event.shaped('kubejs:treebark_cup',[
+        "BB",
+        "BB"
+    ],{
+        B:'farmersdelight:tree_bark'
+    })
+    event.remove({output:'minecraft:crafting_table'})
+    event.shaped('minecraft:crafting_table',[
+        "IA",
+        "PP"
+    ],{
+        I:'minecraft:iron_ingot',
+        A:'create:andesite_alloy',
+        P:'minecraft:oak_planks'
+    })
 })
